3eed804e0fb4e62e6e33ab7d1dd34b835d597afa
accidentlywoo <accidentlywoo@gmail.com>
2020-08-02 18:09:13 +0900

    기본 Component Scan 범위

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloSpringApplication {
-
+// 기본 설정에서는 @SpringBootApplication 가 붙은 Starting Point의 패키지 기준 -> hello.hellospring 하위 패키지의 클래스들만 Component Scanning을한다.
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
