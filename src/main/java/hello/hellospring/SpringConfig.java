83197d9aeac8a20f3f1989fd072f4becb08098bf
accidentlywoo <accidentlywoo@gmail.com>
2020-08-13 12:27:49 +0900

    Project 개발 준비 Setting 대환장 파티

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,18 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.aop.TimeTraceAop;
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.swing.text.html.parser.Entity;
 
 @Configuration
 public class SpringConfig {
@@ -28,22 +16,22 @@ public class SpringConfig {
 //    public SpringConfig(EntityManager em) {
 //        this.em = em;
 //    }
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
+//
+////    @Autowired 생성자 하나 생략가능
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository);
+//    }
 
-//    @Autowired 생성자 하나 생략가능
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository);
-    }
-
-    @Bean
-    public TimeTraceAop timeTraceAop(){
-        return new TimeTraceAop();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository(){
 //        return new MemoryMomberRepository();
